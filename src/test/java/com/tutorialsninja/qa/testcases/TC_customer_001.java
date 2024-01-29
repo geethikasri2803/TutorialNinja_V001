65fb2784be1e674a0ac3cf8800ad568cac07806d
geethikasri <honey0881@gmail.com>
2024-01-29 22:03:48 +0530

    added one file like TC_customer_002 and modified TC_customer_001...

--- src/test/java/com/tutorialsninja/qa/testcases/TC_customer_001.java
+++ src/test/java/com/tutorialsninja/qa/testcases/TC_customer_001.java
@@ -1,1 +1,8 @@
 //this is new test case done by another tester
+
+// this code is added newly 
+// this code is added newly 
+// this code is added newly 
+// this code is added newly 
+// this code is added newly ..... so on i changes the code
+
---
+++ src/test/java/com/tutorialsninja/qa/testcases/TC_customer_002.java
@@ -0,0 +1,7 @@
+package com.tutorialsninja.qa.testcases;
+
+public class TC_customer_002 {
+
+	//this is my new test case i add the new test method
+	
+}
